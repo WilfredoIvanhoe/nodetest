5be933eca05da870e37d551752b646654ac05f46
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-02-05 13:41:35 -0600

    added html

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ http = require('http');
 fs = require('fs');
 var server= http.createServer(function(res,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	fs.createReadStream('./').pipe(res);
-	res.end('<H1>Chinga tu padre Jose</H1>');
+	fs.createReadStream('gorilla.html').pipe(res);
 });
 
 server.listen(Number(process.env.PORT||3000));
