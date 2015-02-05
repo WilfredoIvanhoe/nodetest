b3293c33ed7e54853f2e38f3bf916ce7dcb1df4a
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-02-05 14:46:57 -0600

    Se cambio de demo1.html a index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ http = require('http');
 fs = require('fs');
 var server= http.createServer(function(res,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	fs.createReadStream('gorilla.html').pipe(res);
+	fs.createReadStream('demo1.html').pipe(res);
 });
 
 server.listen(Number(process.env.PORT||3000));
