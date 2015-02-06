c1f6bc627505c72dcb93ed9ee967b2b350b63694
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-02-05 19:29:14 -0600

    deleted vanilla nodejs

--- server.js
+++
@@ -1,8 +0,0 @@
-http = require('http');
-fs = require('fs');
-var server= http.createServer(function(res,res){
-	res.writeHead(200,{'Content-Type':'text/html'});
-	fs.createReadStream('index.html').pipe(res);
-});
-
-server.listen(Number(process.env.PORT||3000));
