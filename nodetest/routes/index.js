f9ccb5f898eaa40a99d40662bb680913bb8e954a
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-02-09 20:06:40 -0600

    s

--- nodetest/routes/index.js
+++ nodetest/routes/index.js
@@ -1,33 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var Schema=mongoose.Schema;
-var numbSchema=new Schema({
-  x:Number
-});
-var Numbs= mongoose.model('Numbers',numbSchema);
+
 /* GET home page. */
 //mongoose.model('Mydb',{x:String});
 router.get('/', function(req, res, next) {
-
-  mongoose.connect('mongodb://wilfredoivanhoe:chichicuilote05@ds045057.mongolab.com:45057/wilfredoivanhoe');
-  var numb=new Numbs({
-  	x:43
-  });
-    numb.save(function(err,numb){
-  	if(err){
-  		res.end(err);
-      console.log(err);
-  		mongoose.disconnect();
-  	}
-  });
-  Numbs.find(function(err,objects){
-    if(err)
-      res.end(err);
-    //res.send(objects);
-    res.render('index', { 'title': 'Lucy the cat' , 'objects': objects});
-    mongoose.disconnect();
-  });
+  res.render('index', { 'title': 'Lucy the cat'});
 });
 
 module.exports = router;
--- nodetest/routes/users.js
+++ nodetest/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Schema=mongoose.Schema;
+var numbSchema=new Schema({
+  x:Number
+});
+var Numbs= mongoose.model('Numbers',numbSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  mongoose.connect('mongodb://wilfredoivanhoe:chichicuilote05@ds045057.mongolab.com:45057/wilfredoivanhoe');
+  var numb=new Numbs({
+  	x:43
+  });
+  /**numb.save(function(err,numb){
+  	if(err){
+  		res.end(err);
+      console.log(err);
+  		mongoose.disconnect();
+  	}
+  });**/
+  Numbs.find(function(err,objects){
+    if(err)
+      res.end(err);
+    //res.send(objects);
+    res.send(objects);
+    mongoose.disconnect();
+  });
+  
 });
 
 module.exports = router;
