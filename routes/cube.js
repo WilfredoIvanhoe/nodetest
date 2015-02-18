f7efb2d498dc95eb4478dba27989e435428c15ae
Jorge <jordan.965@hotmail.com>
2015-02-18 13:48:21 -0600

    J

--- routes/cube.js
+++ routes/cube.js
@@ -5,5 +5,4 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('cube',{});
 });
-
 module.exports = router;
--- routes/galery.js
+++ routes/galery.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('galery',{ title: 'Image-Galery' });
 });
-
+router.get('/layers',function(req,res,next){
+	res.render('layers',{});
+});
 module.exports = router;
